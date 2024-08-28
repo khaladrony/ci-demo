1f454d101373d44f6bbeec3b6234d49b27a1cbf2
Khaled Mosharaf <khaled.mosharaf@bjitgroup.com>
2024-08-28 14:32:56 +0600

    Initial commit

---
+++ src/main/java/com/bjit/ci_demo/CiDemoApplication.java
@@ -0,0 +1,15 @@
+package com.bjit.ci_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiDemoApplication.class, args);
+
+		System.out.println("CI project test");
+	}
+
+}
